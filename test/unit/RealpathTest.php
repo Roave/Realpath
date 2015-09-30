589cda39c90af222c17ffa456aece8fcd2415868
James Titcumb <james@asgrim.com>
2015-09-30 09:08:53 +0100

    Refactored testGetWithNonExistentPathThrowsException slightly to check the specific message

--- test/unit/RealpathTest.php
+++ test/unit/RealpathTest.php
@@ -18,9 +18,16 @@ class RealpathTest extends \PHPUnit_Framework_TestCase
 
     public function testGetWithNonExistentPathThrowsException()
     {
-        $test = "/foo/bar/baz/bat";
+        $shouldFailPath = sys_get_temp_dir() . '/this_folder_must_not_exist';
+        $fullAttemptedPath = $shouldFailPath . '/foo/bar';
 
-        $this->setExpectedException(PathPartDoesNotExist::class);
-        Realpath::get($test);
+        $expectedMessage = sprintf(
+            'Could not find parent path "%s" whilst trying to locate full path "%s"',
+            $shouldFailPath,
+            $fullAttemptedPath
+        );
+
+        $this->setExpectedException(PathPartDoesNotExist::class, $expectedMessage);
+        Realpath::get($fullAttemptedPath);
     }
 }
